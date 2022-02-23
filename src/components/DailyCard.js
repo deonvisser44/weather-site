663f5ce081744a526514ddb4460ae8b28bb60216
deonvisser44 <deonvisser44@gmail.com>
2022-02-23 11:39:07 +0700

    Context Changes

--- src/components/DailyCard.js
+++ src/components/DailyCard.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import classes from './DailyCard.module.css';
+import React from "react";
+import classes from "./DailyCard.module.css";
 
 function DailyCard(props) {
-
-    let d = new Date(props.date*1000);
-    const months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-    let month = months[d.getMonth()];
-    let day = days[d.getDay()];
-    let dayNum = d.getDate();
-
-
-
+  let d = new Date(props.date * 1000);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[d.getDay()];
+  let dayNum = d.getDate();
 
   return (
-    <div className={classes['daily-div']}>
-        <p className={classes.date}>{day} {dayNum}</p>
-        <p className={classes.high}>{Math.round(props.maxTemp)}°</p>
-        <p className={classes.low}>{Math.round(props.minTemp)}°</p>
-        <img className={classes.icon} src={props.icon} alt="icon" />
+    <div className={classes["daily-div"]}>
+      <p className={classes.date}>
+        {day} {dayNum}
+      </p>
+      <p className={classes.high}>{Math.round(props.maxTemp)}°</p>
+      <p className={classes.low}>{Math.round(props.minTemp)}°</p>
+      <img className={classes.icon} src={props.icon} alt="icon" />
     </div>
-  )
+  );
 }
 
-export default DailyCard
+export default DailyCard;
